090c289e423318ac57262f6ccc920898dec9a645
Евгений <gekachan2000@gmail.com>
2019-12-21 14:07:03 +0500

    Сделали аутентификацию

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const hbs = require("express-handlebars");
 const multer  = require('multer');
 const upload = multer();
+const crypto = require("crypto");
 
 const rootDir = process.cwd();
 const port = process.env.PORT || 5000;
@@ -28,23 +29,41 @@ app.engine(
     })
 );
 
-app.get("/", (_, res) => {
-    res.render("html/index.hbs", {
-        layout: "default"
-    });
-});
+const users = {};
 
-app.get("/authTeacher", (_, res) => {
-   res.render("html/authTeacher.hbs", {
-       layout: "default"
-   });
+app.get("/", (req, res) => {
+    if (req.cookies.user) {
+        if (Object.entries(users).length !== 0) {
+            res.redirect("/createLesson");
+        } else {
+            res.render("html/index.hbs", {
+                layout: "default"
+            });
+        }
+    }
 });
 
 app.post("/", upload.none(), (req, res) => {
     const result = req.body;
     const login = result["login"];
     const password = result["password"];
-    console.log(result);
+    if (!Object.keys(users).includes(login)) {
+        users[login] = password;
+    }
+    if (users[login] !== password) {
+        res.send("Неверный пароль или имя пользователя");
+    }
+    res.cookie(
+        "user",
+        crypto.createHash('md5').update(login+password).digest("hex"),
+        {maxAge: 9000000, httpOnly: true});
+    res.redirect("/createLesson");
+});
+
+app.get("/createLesson", (_, res) => {
+    res.render("html/createLesson.hbs", {
+        layout: "default"
+    });
 });
 
 app.post("/", upload.none(), (req, res) => {
